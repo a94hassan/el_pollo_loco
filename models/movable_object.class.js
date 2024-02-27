e7997d35f4f04e67f7170441dec9374b7872767e
Hassan Ammar <h.ammar@hotmail.de>
2024-02-27 23:41:06 +0100

    background added & some basic animations

--- models/movable_object.class.js
+++ models/movable_object.class.js
@@ -1,18 +1,32 @@
 class MovableObject {
     x = 120;
-    y = 250;
+    y = 280;
     img;
     height = 150;
     width = 100;
+    imageCache = {};
+    currentImage = 0;
+    speed = 0.1;
 
     loadImage(path) {
         this.img = new Image(); 
         this.img.src = path;
     }
 
+    loadImages(array) {
+        array.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        })
+    }
+
     moveRight() {
     }
 
-    moveLeft() {       
+    moveLeft() {
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
     }
 }
